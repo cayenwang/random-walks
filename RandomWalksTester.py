49c7237be3474bdb46bebc31287165d7cb6c117d
WontJump <jumpwillem@gmail.com>
2023-07-03 12:45:31 +0100

    Working Random walks generator

--- RandomWalksTester.py
+++ RandomWalksTester.py
@@ -10,36 +10,39 @@ import numpy as np
 class Lattice: 
     def __init__(self, adj, biases):
         self.adjPoints = adj
-        self.biases = biases # this line now no longer works 
-    def bias(self, point): 
+        self.biases = biases  
+    def pointBias(self, point): 
         edgeBias = []
         for i in self.adjPoints(point): 
             edgeBias.append(self.biases(point,i))
         return edgeBias
-
-# This was a basic test case
-# def trivAdj(v):
-#     return 1 
-# trivLat = Lattice(trivAdj,1)
-# print(trivLat.adjPoints(13))
-
+    
 def planeAdj(v):
     x=v[0]; y=v[1] 
     return [[x+1,y],[x-1,y],[x,y+1],[x,y-1]] # This works woo 
 
-plane = Lattice(planeAdj, 1/4) # probably will change to a function which always outputs 0.25
+def planeBias(v,w):
+    return 0.25
+plane = Lattice(planeAdj, planeBias) # probably will change to a function which always outputs 0.25
+
+
+
 
 class RandWalks: 
-    def __init__(self, lattice, length, origin = [0,0]): #origin should be a property of the lattice
+    def __init__(self, lattice, length, origin = [0,0]): #origin should be a property of the lattice, maybe this should be a sub class of lattice 
         self.origin = origin 
         runDir = [origin]
-        runLocation = origin
+        runLocation = [0,0]
+        self.adjPoints = lattice.adjPoints
+        self.pointBias = lattice.pointBias
         for i in range(length): 
-            runLocation =np.random.choice(range(len(plane.adjPoints(runLocation))), plane.bias(runLocation))
+            runLocation =self.adjPoints(runLocation)[int(np.random.choice(range(len(self.adjPoints(runLocation))),1 , self.pointBias(runLocation)))] #Error type of input data not int? This isnt the location its the direction we are moving in 
+            # ahh the problem is runLocation is a number but it then gets fed back into the adjpoints fucntion as sif its a point which it isnt 
             runDir.append(runLocation) 
         self.runDir = runDir
 
-# planeWalk = RandWalks(plane,4)
-# print(planeWalk.runDir)
-# hello 
+
+planeWalk = RandWalks(plane,100)
+print(planeWalk.runDir)
+
 
--- Test.py
+++ Test.py
@@ -1,8 +1,22 @@
-class Test: 
-    def __init__(self,func): 
+import numpy as np 
+class Lattice: 
+    def __init__(self, adj, biases):
+        self.adjPoints = adj
+        self.biases = biases  
+    def pointBias(self, point): 
+        edgeBias = []
+        for i in self.adjPoints(point): 
+            edgeBias.append(self.biases(point,i))
+        return edgeBias
     
-    
-def Arb() : return 1
+def planeAdj(v):
+    x=v[0]; y=v[1] 
+    return [[x+1,y],[x-1,y],[x,y+1],[x,y-1]] # This works woo 
 
-test = Test(Arb)
-test.Func(5)
+def planeBias(v,w):
+    if v == [1,0]: return 4
+    return 0.25
+plane = Lattice(planeAdj, planeBias) # probably will change to a function which always outputs 0.25
+planeBias(23,34)
+plane.biases(1,2)
+plane.pointBias([1,2])
